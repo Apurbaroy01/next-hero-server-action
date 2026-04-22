22a0da215e69a66dd34f37a64670eabb4d57451a
Apurbaroy01 <apurbaroy00011@gmail.com>
2026-04-22 19:36:08 +0600

    fix

--- src/app/task/page.jsx
+++ src/app/task/page.jsx
@@ -7,9 +7,11 @@ const taskPage = async () => {
     const tasks = await getTask();
     return (
         <div>
-            <h2>This is task page</h2>
-            <p>task length {tasks.length}</p>
-            <div className="flex flex-wrap gap-4">
+            <div className="flex flex-col justify-center items-center text-center">
+                <h2 className="font-bold md:text-3xl p-2">This is task page</h2>
+                <p>task length {tasks.length}</p>
+            </div>
+            <div className="flex flex-wrap gap-4 p-4">
                 {tasks.map((task) => (
                     <TaskCard key={task.id} task={task} />
                 ))}
